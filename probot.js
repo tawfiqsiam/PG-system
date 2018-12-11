1cb9beb9f16b0390f1c9b83650ea23051d914f2d
tawfiqsiam <toto.siam999@gmail.com>
2018-12-11 12:51:43 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
 const Canvas = require("canvas"); //npm i canvas
-const prefix = "YOUR Prefix Here" // برفكس
+const prefix = "." // برفكس
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
@@ -824,4 +824,4 @@ message.channel.stopTyping()
 })
 }
 });
-client.login("token Your Bot Here");  // توكن حقق
+client.login("NTIxOTg1NjA2MDA4NTY5ODU3.DvEYow.ODLx1n4j12BTyCAgYP6WHl3rhso");  // توكن حقق
